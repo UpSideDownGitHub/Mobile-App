59633d73ddde38f497b146fb17cbc96707bf8920
UpSideDownGitHub <65664827+UpSideDownGitHub@users.noreply.github.com>
2023-02-22 19:09:21 +0000

    Added login system

--- app/src/main/java/com/example/mobileappas1/MainActivity.java
+++ app/src/main/java/com/example/mobileappas1/MainActivity.java
@@ -3,12 +3,60 @@ package com.example.mobileappas1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    public String[] usernames = {"one", "two", "three", "four", "five",
+                                 "six", "seven", "eight", "nine", "ten"};
+    public String[] passwords = {"one", "two", "three", "four", "five",
+                                 "six", "seven", "eight", "nine", "ten"};
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button loginButton = findViewById(R.id.login_button);
+        loginButton.setOnClickListener( view -> checkLogin());
+    }
+
+    public void checkLogin()
+    {
+        EditText username = findViewById(R.id.username_edittext);
+        EditText password = findViewById(R.id.password_edittext);
+
+        for (int i = 0; i < usernames.length; i++)
+        {
+            if (username.getText().toString().equals(usernames[i]))
+            {
+                for (int j = 0; j < passwords.length; j++)
+                {
+                    if (password.getText().toString().equals(passwords[j]))
+                    {
+                        // both are correct so move to next window
+                        Toast.makeText(
+                                getApplicationContext(),
+                                R.string.welcome,
+                                Toast.LENGTH_LONG).show();
+                        return;
+                    }
+                }
+                //password incorrect
+                Toast.makeText(
+                        getApplicationContext(),
+                        R.string.password_incorrect,
+                        Toast.LENGTH_LONG).show();
+                break;
+            }
+        }
+        // Username Incorrect
+        Toast.makeText(
+                getApplicationContext(),
+                R.string.username_incorrect,
+                Toast.LENGTH_LONG).show();
     }
 }
